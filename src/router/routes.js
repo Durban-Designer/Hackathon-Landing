29c1698d5ae47a804ae8b0af0b75b4df0f9c05a6
Durban-Designer <royce.birnbaum@gmail.com>
2018-05-08 13:13:29 -0700

    fixed

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,7 @@
 
 export default [
-  {path: '/', component: () => import('pages/Home'),
-  {path: '/Register', component: () => import('pages/Register'),
+  { path: '/', component: () => import('pages/Home'),
+  { path: '/Register', component: () => import('pages/Register'),
 
   { // Always leave this as last one
     path: '*',
